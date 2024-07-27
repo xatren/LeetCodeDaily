dc783b9269c9ff063f7477fc0b4071445d2a815e
Emircan Yüksel <emircanyuksel24@gmail.com>
2024-07-27 22:57:29 +0300

    [2704] [Time Beats: 66.58%] [Memory Beats: 37.25%] - LeetPush

---
+++ 2704-To-Be-Or-Not-To-Be.ts
@@ -0,0 +1,20 @@
+function expect(val) {
+    return {
+        toBe: function(expected) {
+            if (val === expected) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(expected) {
+            if (val !== expected) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+}
+
+
