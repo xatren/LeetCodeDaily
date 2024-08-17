c4bb3ba1af166d0015dbece15ab295ca5782601a
Emircan Yüksel <emircanyuksel24@gmail.com>
2024-08-18 01:46:55 +0300

    [1937] [Time Beats: 100.00%] [Memory Beats: 100.00%] - LeetPush

---
+++ DCP-08-24/1937-Maximum-Number-of-Points-with-Cost.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(points), len(points[0])
+        
+        dp = points[0]
+        
+        for i in range(1, m):
+            left = [0] * n
+            right = [0] * n
+            
+            left[0] = dp[0]
+            for j in range(1, n):
+                left[j] = max(left[j-1] - 1, dp[j])
+                
+            right[n-1] = dp[n-1]
+            for j in range(n-2, -1, -1):
+                right[j] = max(right[j+1] - 1, dp[j])
+            
+            for j in range(n):
+                dp[j] = points[i][j] + max(left[j], right[j])
+        
+        return max(dp)
