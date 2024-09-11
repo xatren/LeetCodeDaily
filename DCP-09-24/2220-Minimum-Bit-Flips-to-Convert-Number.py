cbdd38d06a9dc6be7d99afac6d828aac4eb451ff
Emircan Yüksel <emircanyuksel24@gmail.com>
2024-09-11 10:21:18 +0300

    [2220] [Time Beats: 94.20%] [Memory Beats: 64.73%] - LeetPush

---
+++ DCP-09-24/2220-Minimum-Bit-Flips-to-Convert-Number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minBitFlips(self, start, goal):
+        """
+        :type start: int
+        :type goal: int
+        :rtype: int
+        """
+        xor_result = start ^ goal
+        return bin(xor_result).count('1')
