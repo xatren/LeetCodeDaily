307af13a4ad48c38557ba6e9aa16be6753e71abf
Emircan Yüksel <emircanyuksel24@gmail.com>
2025-05-27 10:31:47 +0300

    [2894] [Time Beats: 38.64%] [Memory Beats: 12.37%] - LeetPush

---
+++ DCP-05-25/Divisible-and-Non-divisible-Sums-Difference.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def differenceOfSums(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        num1 = 0 
+        num2 = 0 
+
+        for i in range(1, n + 1):
+            if i % m == 0:
+                num2 += i
+            else:
+                num1 += i
+        return num1 - num2
